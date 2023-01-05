63614dab65c8efec77c0460373688e4a4063d26a
Niolum <al_belov98@mail.ru>
2023-01-05 17:22:04 +0700

    Add docker-compose

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = ''
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fqqw64n@$q)xu&g0sv0w07-^$(q22l%5rusi+h-tils+xv1+e$')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = ['stark-stream-28977.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
